fa746662cb8b7592d83c7924c60ffd7cddaffe95
Igor <31211943+IgorNosach23@users.noreply.github.com>
2017-10-26 14:04:11 +0200

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -9,20 +9,30 @@ import java.util.TreeSet;
 public class Test {
 
     public static void main(String[] args) throws IOException {
-//Write a program to find all distinct words from a text file. Ignore chars like[;.,/-;:] and Ignore case sensitivity.
-        String s[] = new String(Files.readAllBytes(Paths.get("/home/igor/text"))).replaceAll("[.,/-;:]", "").split(" ");
+    //Write a program to find all distinct words from a text file. Ignore chars like[;.,/-;:] and ignore case sensitivity.
+        String allWords [] = new String(Files.readAllBytes(Paths.get("{PATH}")))
+            .replaceAll("[.,/-;:]", "")
+            .split(" ");
+        
+        //Get unique words to ignore case sensitivity. 
+        
         TreeSet<String> treeSet = new TreeSet() {
+            
             @Override
             public Comparator comparator() {
+                
                 return (e, r) -> ((String) e).compareToIgnoreCase((String) r);
+                
             }
         };
-        Collections.addAll(treeSet, s);
+        Collections.addAll(treeSet, allWords);
     }
 
 //Write a generic method that takes an array of objects and a collection, and puts all objects in the array into the collection.
     static <T> void fromArrayToCollection(T[] a, Collection<T> c) {
+        
         Collections.addAll(c, a);
+        
     }
 
 
