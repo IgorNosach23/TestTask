49b3f51013fba93de4506a49c49145b8b99db606
Igor <31211943+IgorNosach23@users.noreply.github.com>
2018-02-07 11:11:34 +0200

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -3,38 +3,30 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.TreeSet;
 
 public class Test {
 
+    //Write a program to find all distinct words from a text file. 
+    // Ignore chars like[;.,/-;:] and ignore case sensitivity.
     public static void main(String[] args) throws IOException {
-    //Write a program to find all distinct words from a text file. Ignore chars like[;.,/-;:] and ignore case sensitivity.
-        String allWords [] = new String(Files.readAllBytes(Paths.get("{PATH}")))
-            .replaceAll("[.,/-;:]", "")
-            .split(" ");
+        String[] words = getFilteredArrayWordsFromFile("pathToFile", "[.,/-;:]");
         
-        //Get unique words to ignore case sensitivity. 
-        
-        TreeSet<String> treeSet = new TreeSet() {
-            
-            @Override
-            public Comparator comparator() {
-                
-                return (e, r) -> ((String) e).compareToIgnoreCase((String) r);
-                
-            }
-        };
-        Collections.addAll(treeSet, allWords);
+        //Get unique words to ignore case sensitivity.
+        TreeSet<String> treeSet = new TreeSet<>((e, r) -> (e).compareToIgnoreCase(r));
+        Collections.addAll(treeSet, words);
     }
 
-//Write a generic method that takes an array of objects and a collection, and puts all objects in the array into the collection.
-    static <T> void fromArrayToCollection(T[] a, Collection<T> c) {
-        
+    private static String[] getFilteredArrayWordsFromFile(String pathToFile, String regex) throws IOException {
+        String[] allWords = new String(Files.readAllBytes(Paths.get(pathToFile)))
+                .replaceAll(regex, "")
+                .split(" ");
+        return allWords.length > 0 ? allWords : new String[]{"Array is empty"};
+    }
+
+    //Write a generic method that takes an array of objects and a collection, and puts all objects in the array into the collection.
+    private static <T> void fromArrayToCollection(T[] a, Collection<T> c) {
         Collections.addAll(c, a);
-        
     }
-
-
 }
 
